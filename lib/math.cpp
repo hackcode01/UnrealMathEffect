80bfc63868bc4fe06dd0bbb987857bceb92436e5
hackcode01 <andreyrazinwork@yandex.ru>
2025-02-07 00:06:23 +0500

    Добавил тестовую структуру проекта.

---
+++ lib/math.cpp
@@ -0,0 +1,17 @@
+#include "./math.hpp"
+
+int_fast32_t add(int_fast32_t a, int_fast32_t b) {
+    return a + b;
+}
+
+int_fast32_t subtract(int_fast32_t a, int_fast32_t b) {
+    return a - b;
+}
+
+int_fast32_t multiplication(int_fast32_t a, int_fast32_t b) {
+    return a * b;
+}
+
+int_fast32_t division(int_fast32_t a, int_fast32_t b) {
+    return a / b;
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include "../lib/math.hpp"
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << add(3, 21) << '\n';
+
+    return 0;
+}
